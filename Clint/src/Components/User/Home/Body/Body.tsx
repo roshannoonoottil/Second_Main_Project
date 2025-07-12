713ce74bd35592add08bb50dab1d2fca153557eb
Roshan Eldho Benny <roshannoonoottil@gmail.com>
2025-07-12 20:05:50 +0530

    auth login validation fixed
    Account is linked to Google  Please use Google login.

--- Clint/src/Components/User/Home/Body/Body.tsx
+++ Clint/src/Components/User/Home/Body/Body.tsx
@@ -2,13 +2,10 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { useNavigate } from 'react-router-dom';
 // import axios from 'axios';
-import { useDispatch} from 'react-redux';
 import { useEffect } from 'react';
-import { logout } from '../../../../Redux/authSlice';
 
 function Body() {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
 
 // interface AuthState {
 //   isAuthenticated: boolean; // true or false
@@ -34,10 +31,6 @@ useEffect(()=>{
 
     <Header/>
     <div><h1>Home Page</h1></div>
-    <button onClick={(()=>{
-      dispatch(logout())
-      localStorage.removeItem('token')
-    })}>logout</button>
     <Footer/>
 
     </div>
